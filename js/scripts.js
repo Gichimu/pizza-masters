1418ead12edc14a616ab8ce1f5153bc8e750890e
Gichimu <gichimueric12@gmail.com>
2019-10-07 09:39:04 +0300

    sort out bugs in the jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     //User interface logic
 
     $("button#submit").click(function(){
+        
         var locale = $("select#inputState3").val();
         var estate = $("input#estate").val();
         var hse = $("input#house_number").val();
@@ -19,16 +20,18 @@ $(document).ready(function(){
         
         var inputSize = $("select#inputState1").val();
         var inputCrust = $("select#inputState2").val();
+        var inputQuantity = $("input#quantity").val();
+        var inputToppings = "";
 
-        var inputToppings = [];
-        $.each($("input[type='checkbox']:checked"), function(){
+        /*$.each($("input[type='checkbox']:checked"), function(){
             inputToppings.push($(this).val());
-        });
+        });*/
 
+        var newPizza = new Pizza(inputSize, inputCrust, inputQuantity, inputToppings)
         
-
+        
         $("p#message1").append("<p>Your pizza will be delivered to "+estate+" estate, house number "+hse+" in "+locale+"</p>");
-        $("p#message2").append("<p>Your total will be: Ksh. "+newPizza.getCost(inputSize, inputCrust, inputToppings)+"</p>");
+        $("p#message2").append("<p>Your total will be: Ksh. "+newPizza.getCost()+"</p>");
 
        
 
@@ -38,7 +41,7 @@ $(document).ready(function(){
 
 
     //Business logic
-    var Pizza = {
+    /*var Pizza = {
         size : {
             small : 100,
             medium : 200,
@@ -54,39 +57,108 @@ $(document).ready(function(){
             extra_cheese : 300,
             veggie : 400
         }
-    }
+    }*/
 
-    function Pizza (size, crust, toppings){
+    function Pizza (size, crust, quantity, toppings){
         this.size = size;
         this.crust = crust;
+        this.quantity = quantity;
         this.toppings = toppings;
     }
 
     Pizza.prototype.getCost = function(){
-        
+        var size = $("select#inputState1").val();
+        var crust = $("select#inputState2").val();
+        var quantity = $("input#quantity").val();
+        var toppings = "";
         var price = 0;
         var delivery = false;
 
-        if(size === "small"){
-            price += this.size.small;
-
-        }else if(size === "medium"){
-            price += this.size.medium;
+        if ($('#inlineCheckbox1').is(":checked"))
+        {
+            toppings = "extra_tomato";
+        }else if($('#inlineCheckbox2').is(":checked")){
+            toppings = "extra_cheese";
+        }else if($('#inlineCheckbox3').is(":checked")){
+            toppings = "veggie";
         }else{
-            price += this.size.large;
+            toppings = "";
         }
 
-        if(crust === "extra_crust"){
-            price += this.crust.extra_crust;
-        }else if(crust === "vegetarian"){
-            price += this.crust.vegetarian;
+        if ($('#show_address').is(":checked"))
+        {
+            delivery = true;
         }else{
-            price += this.crust.regular;
+            delivery = false; 
         }
 
-        toppings.forEach(function(topping){
-            price += this.toppings.topping;
-        })
+        if((size === "small")&&(crust === "regular")&&(toppings === "extra_tomato")){
+            price = 300;
+
+        }else if((size === "small")&&(crust === "regular")&&(toppings === "extra_cheese")){
+            price = 400;
+        }else if((size === "small")&&(crust === "regular")&&(toppings === "veggie")){
+            price = 500;
+        }else if((size === "small")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 500;
+        }else if((size === "small")&&(crust === "extra_crust")&&(toppings === "extra_cheese")){
+            price = 600;
+        }else if((size === "small")&&(crust === "extra_crust")&&(toppings === "veggie")){
+            price = 700;
+        }else if((size === "small")&&(crust === "vegetarian")&&(toppings === "extra_tomato")){
+            price = 600;
+        }else if((size === "small")&&(crust === "vegetarian")&&(toppings === "extra_cheese")){
+            price = 700;
+        }else if((size === "small")&&(crust === "vegetarian")&&(toppings === "veggie")){
+            price = 800;
+        }else if((size === "small")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 500;
+        }else if((size === "medium")&&(crust === "regular")&&(toppings === "extra_tomato")){
+            price = 400;
+
+        }else if((size === "medium")&&(crust === "regular")&&(toppings === "extra_cheese")){
+            price = 500;
+        }else if((size === "medium")&&(crust === "regular")&&(toppings === "veggie")){
+            price = 600;
+        }else if((size === "medium")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 600;
+        }else if((size === "medium")&&(crust === "extra_crust")&&(toppings === "extra_cheese")){
+            price = 700;
+        }else if((size === "medium")&&(crust === "extra_crust")&&(toppings === "veggie")){
+            price = 800;
+        }else if((size === "medium")&&(crust === "vegetarian")&&(toppings === "extra_tomato")){
+            price = 700;
+        }else if((size === "medium")&&(crust === "vegetarian")&&(toppings === "extra_cheese")){
+            price = 800;
+        }else if((size === "medium")&&(crust === "vegetarian")&&(toppings === "veggie")){
+            price = 900;
+        }else if((size === "medium")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 600;
+        }else if((size === "large")&&(crust === "regular")&&(toppings === "extra_tomato")){
+            price = 500;
+
+        }else if((size === "large")&&(crust === "regular")&&(toppings === "extra_cheese")){
+            price = 600;
+        }else if((size === "large")&&(crust === "regular")&&(toppings === "veggie")){
+            price = 700;
+        }else if((size === "large")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 700;
+        }else if((size === "large")&&(crust === "extra_crust")&&(toppings === "extra_cheese")){
+            price = 800;
+        }else if((size === "large")&&(crust === "extra_crust")&&(toppings === "veggie")){
+            price = 900;
+        }else if((size === "large")&&(crust === "vegetarian")&&(toppings === "extra_tomato")){
+            price = 800;
+        }else if((size === "large")&&(crust === "vegetarian")&&(toppings === "extra_cheese")){
+            price = 900;
+        }else if((size === "large")&&(crust === "vegetarian")&&(toppings === "veggie")){
+            price = 1000;
+        }else if((size === "large")&&(crust === "extra_crust")&&(toppings === "extra_tomato")){
+            price = 700;
+        }else{
+            price = 0;
+        }*/
+
 
         if(delivery === true){
             price += 200;
@@ -94,14 +166,9 @@ $(document).ready(function(){
             price += 0;
         }
 
-        return price;
+        return price * quantity;
         
     }
-
-
-    var newPizza = new Pizza(inputSize, inputCrust, inputToppings);
-    
-    
 
 
 });
